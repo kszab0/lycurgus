af6e0ffa9ad77d0afbe0f80ae51dd5dbf15bba48
Kristóf Szabó <kristofszabo@protonmail.com>
2020-04-21 15:20:53 +0300

    Add hash matcher and regexp matcher

---
+++ matcher.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Matcher decides if an input text is matched by its loaded rules
+type Matcher interface {
+	// Load loads rules to be used to match input texts
+	Load(rules []string)
+	Match(text string) bool
+}
+
+// regexpMatcher uses regular expression rules to match input text
+type regexpMatcher struct {
+	regexp *regexp.Regexp
+}
+
+// Load loads regular expression rules
+func (m *regexpMatcher) Load(rules []string) {
+	regexes := strings.Join(rules, "|")
+	regexes += "$"
+	m.regexp = regexp.MustCompile(regexes)
+}
+
+func (m *regexpMatcher) Match(text string) bool {
+	return m.regexp.MatchString(text)
+}
+
+// hashMatcher uses string rules to match input texts exactly
+// or texts with ":443" as suffix
+type hashMatcher struct {
+	hm map[string]int
+}
+
+func (m *hashMatcher) Load(rules []string) {
+	m.hm = make(map[string]int)
+	for _, rule := range rules {
+		m.hm[rule] = 0
+	}
+}
+
+// Match matches an input text exactly or a text with ":443" as suffix
+func (m *hashMatcher) Match(rule string) bool {
+	rule = strings.TrimSuffix(rule, ":443")
+	_, ok := m.hm[rule]
+	return ok
+}
---
+++ matcher_test.go
@@ -0,0 +1,72 @@
+package main
+
+import "testing"
+
+func TestRegexpMatcher(t *testing.T) {
+	rules := []string{
+		"reddit.com",
+		"^stackoverflow.*$",
+		"news.ycombinator.*",
+	}
+
+	tt := []struct {
+		value    string
+		expected bool
+	}{
+		{"reddit.com", true},
+		{"reddit.com:443", true},
+		{"ads.reddit.com", true},
+		{"reddit.eu", false},
+		{"stackoverflow.com", true},
+		{"stackoverflow.com:443", true},
+		{"ads.stackoverflow.com", false},
+		{"news.ycombinator.com", true},
+		{"news.ycombinator.eu", true},
+		{"ycombinator.com", false},
+		{"google.com", false},
+	}
+
+	matcher := regexpMatcher{}
+	matcher.Load(rules)
+
+	for _, tc := range tt {
+		if tc.expected != matcher.Match(tc.value) {
+			t.Errorf("Match '%s' should be: %t", tc.value, tc.expected)
+		}
+	}
+}
+
+func TestHashMatcher(t *testing.T) {
+	rules := []string{
+		"reddit.com",
+		"reddit.com:443",
+		"stackoverflow.com",
+		"news.ycombinator.com",
+	}
+
+	tt := []struct {
+		value    string
+		expected bool
+	}{
+		{"reddit.com", true},
+		{"reddit.com:443", true},
+		{"ads.reddit.com", false},
+		{"reddit.eu", false},
+		{"stackoverflow.com", true},
+		{"stackoverflow.com:443", true},
+		{"ads.stackoverflow.com", false},
+		{"news.ycombinator.com", true},
+		{"news.ycombinator.eu", false},
+		{"ycombinator.com", false},
+		{"google.com", false},
+	}
+
+	matcher := hashMatcher{}
+	matcher.Load(rules)
+
+	for _, tc := range tt {
+		if tc.expected != matcher.Match(tc.value) {
+			t.Errorf("Match '%s' should be: %t", tc.value, tc.expected)
+		}
+	}
+}
