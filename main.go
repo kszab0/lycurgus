3d13d32f4a78b0dbebff2b80f2a302e9797e4ade
Kristóf Szabó <kristofszabo@protonmail.com>
2020-04-28 12:45:33 +0300

    Optional logging to file

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
+	"os"
 )
 
 func main() {
@@ -11,7 +13,18 @@ func main() {
 	blacklistPath := flag.String("blacklist", defaultBlacklistPath, "path to blacklist file")
 	whitelistPath := flag.String("whitelist", defaultWhitelistPath, "path to whitelist file")
 	autostartEnabled := flag.Bool("autostart", defaultAutostartEnabled, "autostart enabled")
+	logFile := flag.String("log", "", "path to log file")
 	flag.Parse()
+
+	if *logFile != "" {
+		file, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer file.Close()
+		wrt := io.MultiWriter(os.Stdout, file)
+		log.SetOutput(wrt)
+	}
 
 	app, err := NewApp(
 		WithBlockerAddress(*proxyAddress),
